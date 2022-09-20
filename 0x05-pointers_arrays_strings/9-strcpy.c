7bd65000582ec69a6e3454584726f1c78a303cd7
ENDURANCE23 <promiseendurance92@gmail.com>
2022-09-20 13:21:11 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,14 +9,13 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	int inc = 0;
+	int i;
 
-	while (*(src + inc) != '\0')
+	for (i =0; *(src + i) != '\0'; i++)
 	{
-		*(dest + inc) = *(src + inc);
-		inc++;
+	dest[i] = *(src + i);
 	}
-	*(dest + inc) = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
