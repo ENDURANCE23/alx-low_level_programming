7bc867fe5f657b7f91e51c529a030e8f792bfe24
ENDURANCE23 <promiseendurance92@gmail.com>
2022-09-20 12:42:24 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,18 +4,17 @@
 * _puts - prints a string
 * @str: string to print
 *
-* Description: prints a string
-* On success: returns no error
+* Return: void
 */
 
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (*(str + i) != '\0')
+	while (str[i] != '\0')
 	{
-		putchar(*(str + i));
+		_putchar(str[i]);
 		i++;
 	}
-	putchar(10);
+	_putchar('\n');
 }
