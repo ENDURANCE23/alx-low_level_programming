55b668d29ecc7bae4549b92e75a639e089057e37
ENDURANCE23 <promiseendurance92@gmail.com>
2022-09-20 12:50:34 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,15 +3,17 @@
 /**
  * _strlen - returns the length of a string
  * @s: string
- *Return: returns length as integer;
+ *Return: void
  */
 
 int _strlen(char *s)
 {
 	int len = 0;
 
-	while (*(s + len) != '\0')
+	while (s[len] != '\0')
+	{
 		len++;
+	}
 
 	return (len);
 }
