eb82e89a2cba2437ec984670b57722f52cd8b011
ENDURANCE23 <promiseendurance92@gmail.com>
2022-09-22 11:19:09 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -28,7 +28,8 @@ char *leet(char *str)
 
 char transform(char x)
 {
-	char mapping_low[8] = {'o', '1', '\0', 'e', 'a', '\0', '\0', '\0', 't'};
+	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
+	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
 	int i = 0;
 	char replacement = x;
 
