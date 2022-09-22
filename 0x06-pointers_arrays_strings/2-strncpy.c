a618950564c9173505772b29a5e2996f2ac3e65a
ENDURANCE23 <promiseendurance92@gmail.com>
2022-09-22 15:15:42 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,7 +8,7 @@
  *@dest: buffer storing the string copy
  *src: the source string
  *@n: max number of byte copied
- *Return: returns
+ *Return: pointer to the resulting string
  */
 
 char *_strncpy(char *dest, char *src, int n)
